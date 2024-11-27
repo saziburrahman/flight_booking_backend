2364dbab00c038d9d440b93d3e71cff0cd5662df
sazib-dev <sazib@qcoom.com>
2024-11-27 23:47:47 +0600

    user auth functionality

---
+++ controllers/authController.js
@@ -0,0 +1,56 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+exports.register = async (req, res) => {
+  const { username, email, password, confirmPassword } = req.body;
+
+  if (!username || !email || !password || !confirmPassword) {
+    return res.status(400).json({ message: "Please fill in all fields" });
+  }
+  if (password !== confirmPassword) {
+    return res.status(400).json({ message: "Passwords do not match" });
+  }
+
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ msg: "Email already exists" });
+    }
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    res.status(201).json({ message: "User registered successfully", user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d",
+      }
+    );
+    const userResponse = user.toObject();
+    delete userResponse.password;
+
+    res.status(200).json({ token, user: userResponse });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
+const cors = require("cors");
+const { errorHandler } = require("./middleware/errorMiddleware");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 connectDB();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/auth", authRoutes);
+
+app.use(errorHandler);
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
