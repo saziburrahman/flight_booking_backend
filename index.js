35978227df6be5b1e89d0ca20011a0389ea814e1
sazib-dev <sazib@qcoom.com>
2024-12-02 22:14:54 +0600

    cors origin bug fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
-const connectDB = require("./config/db");
 const cors = require("cors");
+const connectDB = require("./config/db");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const authRoutes = require("./routes/authRoutes");
 const flightRoutes = require("./routes/flightRoutes");
@@ -9,12 +9,11 @@ const bookingRoutes = require("./routes/bookingRoutes");
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(
   cors({
-    origin: "https://flight-booking-app-2024.netlify.app",
+    origin: ["https://flight-booking-app-2024.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
@@ -26,12 +25,16 @@ app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Test route
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
 app.use("/api/auth", authRoutes);
 app.use("/api/flights", flightRoutes);
 app.use("/api/bookings", bookingRoutes);
 
 app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
