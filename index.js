6083c21d6d13c77035566b84265e404511f2e421
sazib-dev <sazib@qcoom.com>
2024-12-02 18:25:17 +0600

    hello world test api make

--- index.js
+++ index.js
@@ -13,14 +13,17 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(cors({
-  origin: '*', // Allows all origins
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Explicitly allow these methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/",(req,res)=>{
+  res.send("Hello World")
+})
 app.use("/api/auth", authRoutes);
 app.use("/api/flights", flightRoutes);
 app.use("/api/bookings", bookingRoutes);
