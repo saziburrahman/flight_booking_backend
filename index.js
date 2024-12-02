1405bb5cef22878f2e9d35f1f660e5174cfb189d
sazib-dev <sazib@qcoom.com>
2024-12-02 21:38:10 +0600

    change vercel and cors settings

--- index.js
+++ index.js
@@ -12,18 +12,23 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 connectDB();
 
-app.use(cors({
-  origin: 'https://flight-booking-app-2024.netlify.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(
+  cors({
+    origin: "https://flight-booking-app-2024.netlify.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
+app.options("*", cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/",(req,res)=>{
-  res.send("Hello World")
-})
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 app.use("/api/auth", authRoutes);
 app.use("/api/flights", flightRoutes);
 app.use("/api/bookings", bookingRoutes);
